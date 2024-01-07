a22ee34b921290f92187346e65e79734d8086712
Arnob Tanvir <arnob0188@gmail.com>
2024-01-07 10:33:00 +0600

    showing the items on the index page

--- core/views.py
+++ core/views.py
@@ -1,10 +1,17 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.template import loader
+from item.models import Category, Item
 
 
 def index(request):
-    template = loader.get_template('core/index.html')
-    return HttpResponse(template.render())
+    items = Item.objects.filter(is_sold=False)[0:6]
+    categories = Category.objects.all()
+
+    return render(request, 'core/index.html', {
+        'categories': categories,
+        'items': items,
+    })
 
 
 def contact(request):
--- puddle/urls.py
+++ puddle/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+# to get the image in the front page(we should not do this)
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from core.views import index, contact
 
@@ -22,4 +25,4 @@ urlpatterns = [
     path('', index, name='index'),
     path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
