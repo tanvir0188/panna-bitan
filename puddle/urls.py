4e5ef3a95e889a1e1ce0117dffbbbda745b6c25b
Arnob Tanvir <arnob0188@gmail.com>
2024-01-08 10:07:35 +0600

    details, click an item and the details and related items of that page shows up

--- puddle/urls.py
+++ puddle/urls.py
@@ -15,14 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-# to get the image in the front page(we should not do this)
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path
+from django.urls import path, include
 from core.views import index, contact
 
 urlpatterns = [
     path('', index, name='index'),
     path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
+    path('items/', include('item.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
