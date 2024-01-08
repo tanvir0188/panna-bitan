f8726461cbcd04feac29fe373808f3697bb89f3f
Arnob Tanvir <arnob0188@gmail.com>
2024-01-08 10:27:14 +0600

    re-organizing the urls(taking the index and contact urls to core app

--- puddle/urls.py
+++ puddle/urls.py
@@ -18,11 +18,10 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
-from core.views import index, contact
+
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('contact/', contact, name='contact'),
+    path('', include('core.urls')),
     path('admin/', admin.site.urls),
     path('items/', include('item.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
