f87f4c6ca5dd480b8ed84c9f297b280f2baaff87
Arnob Tanvir <arnob0188@gmail.com>
2024-01-08 11:16:11 +0600

    Signup page done

--- core/urls.py
+++ core/urls.py
@@ -6,5 +6,6 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('contact/', views.contact, name='contact')
+    path('contact/', views.contact, name='contact'),
+    path('signup/', views.signup, name='singup')
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.template import loader
 from item.models import Category, Item
+from .forms import SignupForm
 
 
 def index(request):
@@ -17,3 +18,18 @@ def index(request):
 def contact(request):
     template = loader.get_template('core/contact.html')
     return HttpResponse(template.render())
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('/login/')
+    else:
+        form = SignupForm()
+
+    return render(request, 'core/signup.html', {
+        'form': form
+    })
