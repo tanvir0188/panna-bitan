8a59eb7c24af1d327221b5eea6feff4218f90193
Arnob Tanvir <arnob0188@gmail.com>
2024-01-09 11:14:06 +0600

    Dashboard done(showing the user created items)

--- puddle/urls.py
+++ puddle/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('core.urls')),
     path('admin/', admin.site.urls),
     path('items/', include('item.urls')),
+    path('dashboard/', include('dashboard.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
