aabf9b1fd6ebaa2a1207879da04a2d055a592484
Arnob Tanvir <arnob0188@gmail.com>
2024-01-09 10:59:13 +0600

    item addition and styling done

--- item/views.py
+++ item/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Item
 from .forms import NewItemForm
@@ -17,7 +17,17 @@ def detail(request, pk):
 
 @login_required
 def new(request):
-    form = NewItemForm()
+    if request.method == 'POST':
+        form = NewItemForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.created_by = request.user
+            item.save()
+
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = NewItemForm()
 
     return render(request, 'item/form.html', {
         'form': form,
