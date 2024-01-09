3518677fa8d83ae704e6ddba1fadcc7c91e64de9
Arnob Tanvir <arnob0188@gmail.com>
2024-01-09 10:30:20 +0600

    adding new item

--- item/urls.py
+++ item/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'item'
 
 urlpatterns = [
-    path('<int:pk>/', views.detail, name='detail')
+    path('new/', views.new, name='new'),
+    path('<int:pk>/', views.detail, name='detail'),
 ]
--- item/views.py
+++ item/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-
+from django.contrib.auth.decorators import login_required
 from .models import Item
+from .forms import NewItemForm
 
 
 def detail(request, pk):
@@ -11,4 +12,14 @@ def detail(request, pk):
     return render(request, 'item/detail.html', {
         'item': item,
         'related_items': related_items
+    })
+
+
+@login_required
+def new(request):
+    form = NewItemForm()
+
+    return render(request, 'item/form.html', {
+        'form': form,
+        'title': 'New item',
     })
