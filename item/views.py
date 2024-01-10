cef0a401c52356a57ab240b2e602af38a38e9412
Arnob Tanvir <arnob0188@gmail.com>
2024-01-10 10:05:38 +0600

    Searching and filtering done

--- item/views.py
+++ item/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
 from django.contrib.auth.decorators import login_required
-from .models import Item
+from .models import Item, Category
 from .forms import NewItemForm, EditItemForm
+
+
+def browse(request):
+    query = request.GET.get('query', '')
+    items = Item.objects.filter(is_sold=False)
+    category_id = request.GET.get('category', 0)
+    categories = Category.objects.all()
+
+    if category_id:
+        items = items.filter(category_id=category_id)
+
+    if query:
+        items = items.filter(Q(name__icontains=query) |
+                             Q(description__icontains=query))
+
+    return render(request, 'item/items.html', {
+        'items': items,
+        'query': query,
+        'categories': categories,
+        'category_id': category_id,
+
+    })
 
 
 def detail(request, pk):
