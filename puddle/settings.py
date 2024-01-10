0ddf4fe511669b0d374767c23b44d75f95ffe46d
Arnob Tanvir <arnob0188@gmail.com>
2024-01-10 13:04:52 +0600

    linking with the contact page and setting up the models for conversations done

--- puddle/settings.py
+++ puddle/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'core',
     'dashboard',
     'item',
+    'conversation',
 ]
 
 MIDDLEWARE = [
--- puddle/urls.py
+++ puddle/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('items/', include('item.urls')),
     path('dashboard/', include('dashboard.urls')),
+    path('inbox/', include('conversation.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
